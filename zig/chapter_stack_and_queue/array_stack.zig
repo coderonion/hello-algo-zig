12cc8c57981b11100c58af181076b8d09cd29f9e
sjinzh <99076655+sjinzh@users.noreply.github.com>
2023-02-06 22:14:52 +0800

    Refine some details and coding style for Rust codes

--- zig/chapter_stack_and_queue/array_stack.zig
+++ zig/chapter_stack_and_queue/array_stack.zig
@@ -6,7 +6,6 @@ const std = @import("std");
 const inc = @import("include");
 
 // 基于数组实现的栈
-// 编译期泛型
 pub fn ArrayStack(comptime T: type) type {
     return struct {
         const Self = @This();
@@ -32,13 +31,13 @@ pub fn ArrayStack(comptime T: type) type {
         }
 
         // 判断栈是否为空
-        pub fn empty(self: *Self) bool {
+        pub fn is_empty(self: *Self) bool {
             return self.size() == 0;
         }
 
         // 访问栈顶元素
-        pub fn top(self: *Self) T {
-            if (self.size() == 0) @panic("栈为空");
+        pub fn peek(self: *Self) T {
+            if (self.is_empty()) @panic("栈为空");
             return self.stack.?.items[self.size() - 1];
         }  
 
@@ -62,10 +61,6 @@ pub fn ArrayStack(comptime T: type) type {
 
 // Driver Code
 pub fn main() !void {
-    // 查看本地CPU架构和操作系统信息
-    var native_target_info = try std.zig.system.NativeTargetInfo.detect(std.zig.CrossTarget{});
-    std.debug.print("Native Info: CPU Arch = {}, OS = {}\n", .{native_target_info.target.cpu.arch, native_target_info.target.os.tag});
-
     // 初始化栈
     var stack = ArrayStack(i32){};
     stack.init(std.heap.page_allocator);
@@ -82,11 +77,11 @@ pub fn main() !void {
     inc.PrintUtil.printList(i32, stack.toList());
 
     // 访问栈顶元素
-    var top = stack.top();
-    std.debug.print("\n栈顶元素 top = {}", .{top});
+    var peek = stack.peek();
+    std.debug.print("\n栈顶元素 peek = {}", .{peek});
 
     // 元素出栈
-    top = stack.pop();
+    var top = stack.pop();
     std.debug.print("\n出栈元素 pop = {}，出栈后 stack = ", .{top});
     inc.PrintUtil.printList(i32, stack.toList());
 
@@ -95,7 +90,7 @@ pub fn main() !void {
     std.debug.print("\n栈的长度 size = {}", .{size});
 
     // 判断栈是否为空
-    var is_empty = stack.empty();
+    var is_empty = stack.is_empty();
     std.debug.print("\n栈是否为空 = {}", .{is_empty});
 
     _ = try std.io.getStdIn().reader().readByte();
